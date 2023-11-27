6a016dc46eb45e0e5426eba285220bc4a5821d23
NIKKnak <nik88350759@yandex.ru>
2023-11-27 12:11:25 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharp_course_Lesson_and_DZ_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
